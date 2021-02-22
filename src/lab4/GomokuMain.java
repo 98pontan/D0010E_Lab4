a24befd459cb956fc47d4481155a0041778f8cc3
Pontus Jirbratt <pontus.jirbratt@hotmail.com>
2021-02-22 13:36:06 +0100

    first commit

---
+++ src/lab4/GomokuMain.java
@@ -0,0 +1,25 @@
+package lab4;
+
+import lab4.client.GomokuClient;
+import lab4.data.GomokuGameState;
+import lab4.gui.GomokuGUI;
+
+public class GomokuMain
+{
+   public static void main(String[] args)
+   {
+	  GomokuClient client;
+      GomokuGameState state;
+      int portNumber;
+
+      if (args.length == 1)
+         portNumber = Integer.parseInt(args[0]);
+
+      else
+         portNumber = 4001;
+
+      client = new GomokuClient(portNumber);
+      state = new GomokuGameState(client);
+      new GomokuGUI(state, client);
+   }
+}
